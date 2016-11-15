76c9d7067ffeaabe44041f2766c40e7120d3bd74
Torusrxxx <torusrxxx@users.noreply.github.com>
2016-11-15 10:08:09 +0000

    Create resource.h

---
+++ resource.h
@@ -0,0 +1,19 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ 生成的包含文件。
+// 供 Strings.rc 使用
+//
+#define IDS_JMPFROM                     101
+#define IDS_CALLFROM                    102
+#define IDS_REFFROM                     103
+#define IDS_PLUGINAME                   104
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        102
+#define _APS_NEXT_COMMAND_VALUE         40001
+#define _APS_NEXT_CONTROL_VALUE         1001
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
